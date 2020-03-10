8509c930361765ac239a053b6fb31f04b737ca84
Finedand <aveljic.1995@gmail.com>
2020-03-10 21:01:10 +0100

    Removed some code

--- hello_world.py
+++ hello_world.py
@@ -1,18 +1,7 @@
-import math
-import sys
-from os import rename
-
 import requests
 
-test = "test"
 
-print(sys.version)
-print(sys.executable)
-# print('worlddsda')
 r = requests.get("https://coreyms.com")
 print(r.status_code)
 
-
-def greet(who_to_greet):
-    greeting = f"hello, {who_to_greet}!"
-    return greeting
+print(r.ok)
